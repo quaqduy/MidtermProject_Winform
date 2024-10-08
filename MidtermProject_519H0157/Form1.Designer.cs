f43d89e88d0ed360a9699993e65332d5a1adce63
Quang Duy <87593754+quaqduy@users.noreply.github.com>
2024-10-08 16:10:49 +0700

    Add project files.

---
+++ MidtermProject_519H0157/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace MidtermProject_519H0157
+{
+    partial class loginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(loginForm));
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(232, 182);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(337, 23);
+            this.txtEmail.TabIndex = 0;
+            this.txtEmail.Text = "Enter your email";
+            this.txtEmail.Click += new System.EventHandler(this.txtEmail_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(184)))), ((int)(((byte)(167)))));
+            this.lblTitle.Font = new System.Drawing.Font("Ink Free", 100F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblTitle.Location = new System.Drawing.Point(142, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(526, 166);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "LOGIN";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(232, 208);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(337, 23);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.Text = "Enter your password";
+            this.txtPassword.Click += new System.EventHandler(this.txtPassword_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(184)))), ((int)(((byte)(167)))));
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.ForeColor = System.Drawing.Color.Red;
+            this.lblMessage.Location = new System.Drawing.Point(232, 234);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(337, 15);
+            this.lblMessage.TabIndex = 3;
+            this.lblMessage.Text = "Message";
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(185)))), ((int)(((byte)(163)))));
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnLogin.Location = new System.Drawing.Point(342, 252);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Padding = new System.Windows.Forms.Padding(5);
+            this.btnLogin.Size = new System.Drawing.Size(80, 38);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Sign in";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // loginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::MidtermProject_519H0157.Properties.Resources.loginBg;
+            this.ClientSize = new System.Drawing.Size(762, 462);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.txtEmail);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "loginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
---
+++ MidtermProject_519H0157/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace MidtermProject_519H0157
+{
+    public partial class loginForm : Form
+    {
+
+        private Rectangle screenSize = Screen.PrimaryScreen.Bounds;                 // Get the dimensions of the primary screen
+        SqlConnection conn = new SqlConnection(
+            @"Data Source=QUAQDUY;Initial Catalog=PiStoreDB;Integrated Security=True"
+        );
+
+
+        public loginForm()
+        {
+            InitializeComponent();
+
+            //View handle
+            CenterLabel(lblTitle, 85); // Center title label
+            CenterTextbox(txtEmail,280); // Center message label
+            CenterTextbox(txtPassword, 310); // Center message label
+            CenterLabel(lblMessage, 340); // Center message label
+            CenterBtn(btnLogin, 360); // Center message label
+            placeHolderTextBox(txtEmail, "Enter your email");
+            placeHolderTextBox(txtPassword, "Enter your password");
+            //hide msg
+            lblMessage.Visible = false;
+        }
+
+        private void CenterLabel(Label label, int height)
+        {
+            // Calculate the center position
+            int x = (screenSize.Width - label.Width) / 2 - 500; // Center X
+            int y = height; // Center Y
+            label.Location = new Point(x, y);
+        }
+
+        private void CenterTextbox(TextBox textBox, int height)
+        {
+            // Calculate the center position
+            int x = (screenSize.Width - textBox.Width) / 2 - 500; // Center X
+            int y = height; // Center Y
+            textBox.Location = new Point(x, y);
+        }
+        private void CenterBtn(Button btn, int height)
+        {
+            // Calculate the center position
+            int x = (screenSize.Width - btn.Width) / 2 - 500; // Center X
+            int y = height; // Center Y
+            btn.Location = new Point(x, y);
+        }
+
+        private void SetFontSizes()
+        {
+            // Set font size for controls
+            lblTitle.Font = new Font("Ink Free", 50, FontStyle.Bold); // Title label
+            txtEmail.Font = new Font("Arial", 30); // Username textbox
+            txtPassword.Font = new Font("Arial", 30); // Password textbox
+            btnLogin.Font = new Font("Arial", 30); // Login button
+            lblMessage.Font = new Font("Arial", 20); // Message label
+        }
+
+        private void placeHolderTextBox(TextBox textBox, string text)
+        {
+            // Initialize the placeholder label
+            textBox.Text = text;
+            textBox.ForeColor = Color.Gray; // Set placeholder text color
+            textBox.BringToFront(); // Ensure label is in front of textbox
+            textBox.Visible = true; // Show placeholder initially
+        }
+
+        private void txtEmail_Click(object sender, EventArgs e)
+        {
+            if (txtEmail.Text == "Enter your email")
+            {
+                txtEmail.Text = ""; // Clear the placeholder when focused
+            }
+        }
+
+        private void txtPassword_Click(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == "Enter your password")
+            {
+                txtPassword.Text = ""; // Clear the placeholder when focused
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string msg = loginAuth(txtEmail, txtPassword);
+            if (!string.IsNullOrEmpty(msg))
+            {
+                lblMessage.Visible = true;
+                lblMessage.Text = msg;
+            }
+
+            MessageBox.Show("Sucess to login ");
+        }
+
+        public string loginAuth(TextBox txtEmail, TextBox txtPassword)
+        {
+            string patternPassword = @"^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{8,}$"; // Regex pattern
+            string patternemail = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"; // Regex pattern
+            
+            string email = txtEmail.Text;
+            string password = txtPassword.Text;
+            string msg = "";
+            if (string.IsNullOrEmpty(email))
+            {
+                msg = "Please enter email";
+            }
+            else if (string.IsNullOrEmpty(password))
+            {
+                msg = "Please enter password";
+            }
+            else if (!Regex.IsMatch(email, patternemail))
+            {
+                msg = "Invalid email format.";
+            }
+            else if (!Regex.IsMatch(password, patternPassword))
+            {
+                msg = "Invalid password format.";
+            }
+            else if(!checkedAccountDB(email, password))
+            {
+                msg = "Your email or password is incorrect !!";
+            }
+
+            return msg;
+        }
+
+        private Boolean checkedAccountDB(string email, string password)
+        {
+            // Hash the provided password
+            string hashedPassword = HashPassword(password).ToUpper(); // Use the hashing method defined earlier
+
+            // Prepare the SQL query
+            string query = "SELECT COUNT(*) FROM Account WHERE Email = @Email AND Password = @Password"; // Ensure the table name and fields are correct
+
+            using (SqlCommand command = new SqlCommand(query, conn))
+            {
+                command.Parameters.AddWithValue("@Email", email);
+                command.Parameters.AddWithValue("@Password", hashedPassword); // Use the hashed password for comparison
+
+                try
+                {
+                    conn.Open(); // Open the connection
+
+                    int count = (int)command.ExecuteScalar(); // Execute the query and get the result
+                    return count > 0; // Return true if a matching record exists
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show("Database error: " + ex.Message);
+                    return false;
+                }
+                finally
+                {
+                    conn.Close(); // Ensure the connection is closed
+                }
+            }
+        }
+
+        //Hash
+        private string HashPassword(string password)
+        {
+            // Use SHA256 hashing
+            using (var sha256 = System.Security.Cryptography.SHA256.Create())
+            {
+                // Convert the password string to a byte array and compute the hash
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                // Convert the byte array to a hexadecimal string
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in bytes)
+                {
+                    builder.Append(b.ToString("x2")); // Convert byte to hex string
+                }
+
+                return builder.ToString(); // Return the hashed password
+            }
+        }
+
+    }
+}
